ad50e0a3e8f9d2b09a9886ce8ccb05cc38e0ab80
Jairo Rubio <rh.jairo.94@gmail.com>
2022-01-15 22:44:19 -0600

    added API functionality

--- script.js
+++ script.js
@@ -5,11 +5,14 @@ let hour = myDate.getHours();
 let uppColors = ['#050F65','#A4F0FF','#8080ff','#FF95DF'];
 let lowColors = ['#6F2875','#FFB05A','#80ffdf','#FFA748'];
 
-let root = document.querySelector('.elements');
+const root = document.querySelector('.elements');
+const loc = document.querySelector('.city');
+const tempC = document.querySelector('.measure');
+const weather = document.querySelector('.weather');
 
 let weekday = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
 
-document.getElementById("weekday").innerHTML = weekday[day];
+
 
 function changeColor(){
     if(hour>=19 && hour<=24){
@@ -30,4 +33,43 @@ function changeColor(){
     }
 }
 
-document.onload = changeColor();
+
+const api = "75cb5e4667553ccfd80fbb21f8ba6aee";
+
+window.addEventListener('load', () => {
+    let long;
+    let lat;
+    // Accesing Geolocation of User
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition((position) => {
+        // Storing Longitude and Latitude in variables
+        long = position.coords.longitude;
+        lat = position.coords.latitude;
+        const base = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=${api}&units=metric`;
+  
+        // Using fetch to get data
+        fetch(base)
+          .then((response) => {
+            return response.json();
+          })
+          .then((data) => {
+            console.log(data);
+            const { temp } = data.main;
+            const place = data.name;
+            const { main } = data.weather[0];
+            
+            const fahrenheit = (temp * 9) / 5 + 32;
+
+            loc.textContent = `${place}`;
+            tempC.textContent = `${temp}`;
+            weather.textContent = `${main}`;
+
+            document.getElementById("weekday").innerHTML = weekday[day];
+            changeColor();
+          });
+      });
+    }
+  });
+
+  
+
